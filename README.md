9c99e4f1887c25acd5ee0c71945dc14addea3646
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2018-08-24 11:35:57 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,355 @@
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+# canvas
+
+A simple object to draw on an HTML canvas.
+
+## Node Repository
+
+[https://www.npmjs.com/package/@pelevesque/canvas](https://www.npmjs.com/package/@pelevesque/canvas)
+
+## Example
+
+https://rawgit.com/pelevesque/canvas/master/index.html
+
+## Installation
+
+`npm install @pelevesque/canvas`
+
+## Tests
+
+### Standard Style
+
+`npm test`
+
+## Methods
+
+- `setOptions` sets options
+- `resize` resizes the canvas
+- `clear` clears the canvas
+- `loadImage` loads an image
+- `loadImages` loads multiple images
+- `image` draws an image
+- `imageResized` draws an image with resizing
+- `imageClipped` draws an image with clipping
+- `text` draws text
+- `textCompressed` draws text with a max width
+- `line` draws a line
+- `lines` draws multiple lines
+- `polyline` draws a line with many segments
+- `quadraticCurve` draws a quadratic curve
+- `polygon` draws a polygon
+- `circle` draws a circle
+- `rectangle` draws a rectangle
+- `square` draws a square
+- `getMousePosition` gets the mouse position
+
+## Usage
+
+### Initialization
+
+When initializing canvas, you must provide it with the HTML `id` of the canvas you wish to use. Optionally, you may also override some default `params` by passing an object as the second argument.
+
+```js
+// Initialize canvas without params.
+const canvas = new Canvas(id)
+```
+
+```js
+// Initialize canvas with overriding some params.
+const canvas = new Canvas(id, {
+    'backgroundColor' : '#555555',
+    'color'           : '#ffffff'
+})
+```
+
+```js
+// List of default params.
+defaultParams = {
+  'globalCompositeOperationDefault': 'source-over',
+  'backgroundColor': 'none',
+  'backgroundImage': 'none',
+  'color': '#000000',
+  'border': 'none',
+  'font': '16px Arial',
+  'textAlign': 'center',
+  'textBaseline': 'middle',
+  'cursor': 'default'
+}
+```
+
+### Set Options
+
+Sets options.
+
+```js
+const options = {
+  'backgroundColor' : '#336600',
+  'border' : '1px solid #006600',
+}
+canvas.setOptions(options)
+```
+
+### Resize
+
+Resizes the canvas.
+
+```js
+const width = 500
+const height = 200
+canvas.resize(width, height)
+```
+
+### Clear
+
+Clears the canvas.
+
+```js
+canvas.clear()
+```
+
+### Load Image
+
+Loads an image.
+
+```js
+canvas.loadImage(src)
+```
+
+### Load Images
+
+Loads multiple images.
+
+```js
+canvas.loadImages(sources)
+```
+
+### Image
+
+Draws an image.
+
+#### required params
+
+```js
+canvas.image(src, x, y)
+```
+
+#### with optional params
+
+```js
+canvas.image(src, x, y, globalCompositeOperation)
+```
+
+### Image Resized
+
+Draws an image with resizing.
+
+#### required params
+
+```js
+canvas.imageResized(src, x, y, width, height)
+```
+
+#### with optional params
+
+```js
+canvas.imageResized(src, x, y, width, height, globalCompositeOperation)
+```
+
+### Image Resized
+
+Draws an image with clipping.
+
+#### required params
+
+```js
+canvas.imageClipped(src, sx, sy, swidth, sheight, x, y, width, height)
+```
+
+#### with optional params
+
+```js
+canvas.imageClipped(src, sx, sy, swidth, sheight, x, y, width, height, globalCompositeOperation)
+```
+
+### Text
+
+Draws text.
+
+`fillStyle`, `font`, `textAlign`, and `textBaseline` are optional.
+
+```js
+const text = 'hello cruel world!'
+const x = 10
+const y = 10
+const fillStyle = '#444444'
+const font = '24px Helvetica'
+const textAlign = 'left'
+const textBaseline = 'bottom'
+canvas.text(text, x, y, fillStyle, font, textAlign, textBaseline)
+```
+
+### Text Compressed
+
+Draws text with a max width.
+
+`fillStyle`, `font`, `textAlign`, and `textBaseline` are optional.
+
+```js
+const text = 'hello cruel world!'
+const x = 10
+const y = 10
+const maxWidth = 30
+const fillStyle = '#444444'
+const font = '24px Helvetica'
+const textAlign = 'left'
+const textBaseline = 'bottom'
+canvas.textCompressed(text, x, y, maxWidth, fillStyle, font, textAlign, textBaseline)
+```
+
+### Line
+
+Draws a line.
+
+`strokeStyle` and `strokeWidth` are optional.
+
+```js
+const x1 = 10
+const y1 = 10
+const x2 = 100
+const y2 = 120
+const strokeStyle = '#666666'
+const strokeWidth = '2'
+canvas.line(x1, y1, x2, y2, strokeStyle, strokeWidth)
+```
+
+### Lines
+
+Draws multiple lines.
+
+`strokeStyle` and `strokeWidth` are optional.
+
+```js
+const lines = [
+  [10, 10, 100, 100],
+  [25, 25, 200, 120],
+  [30, 30, 400, 300]  
+]
+const strokeStyle = '#666666'
+const strokeWidth = '2'
+canvas.lines(lines, strokeStyle, strokeWidth)
+```
+
+### Polyline
+
+Draws a line with many segments.
+
+`strokeStyle` and `strokeWidth` are optional.
+
+```js
+const lines = [
+  [10, 10],
+  [25, 25],
+  [200, 150],
+  [120, 50]
+]
+const strokeStyle = '#666666'
+const strokeWidth = '2'
+canvas.polyline(lines, strokeStyle, strokeWidth)
+```
+
+### Quadratic Curve
+
+Draws a quadratic curve.
+
+`strokeStyle` and `strokeWidth` are optional.
+
+```js
+const x1 = 10
+const y1 = 10
+const x2 = 100
+const y2 = 120
+const qx = 50
+const qy = 100
+const strokeStyle = '#666666'
+const strokeWidth = '2'
+canvas.quadraticCurve(x1, y1, x2, y2, qx, qy, strokeStyle, strokeWidth)
+```
+
+### Polygon
+
+Draws a polygon.
+
+`fillStyle`, `strokeStyle`, and `strokeWidth` are optional.
+
+```js
+const coords = [
+  [10, 10],
+  [200, 20],
+  [130, 100],
+  [45, 70]
+]
+const fillStyle = '#ff0000'
+const strokeStyle = '#666666'
+const strokeWidth = '2'
+canvas.polygon(coords, fillStyle, strokeStyle, strokeWidth)
+```
+
+### Circle
+
+Draws a circle.
+
+`fillStyle`, `strokeStyle`, and `strokeWidth` are optional.
+
+```js
+const cx = 10
+const cy = 10
+const radius = 40
+const fillStyle = '#ff0000'
+const strokeStyle = '#666666'
+const strokeWidth = '2'
+canvas.circle(cx, cy, radius, fillStyle, strokeStyle, strokeWidth)
+```
+
+### Rectangle
+
+Draws a rectangle.
+
+`fillStyle`, `strokeStyle`, and `strokeWidth` are optional.
+
+```js
+const x = 15
+const y = 15
+const w = 100
+const h = 200
+const fillStyle = '#ff0000'
+const strokeStyle = '#666666'
+const strokeWidth = '2'
+canvas.rectangle(x, y, w, h, fillStyle, strokeStyle, strokeWidth)
+```
+
+### Square
+
+Draws a square.
+
+`fillStyle`, `strokeStyle`, and `strokeWidth` are optional.
+
+```js
+const x = 25
+const y = 300
+const w = 100
+const fillStyle = '#ff0000'
+const strokeStyle = '#666666'
+const strokeWidth = '2'
+canvas.square(x, y, w, fillStyle, strokeStyle, strokeWidth)
+```
+
+### Get Mouse Position
+
+Gets the mouse position.
+
+```js
+const mousePosition = canvas.getMousePosition()
+const x = mousePosition.x
+const y = mousePosition.y
+```
---
+++ bundle.js
@@ -0,0 +1,221 @@
+/* global event, Image */
+'use strict'
+
+const Canvas = function (id, options) {
+  this.canvas = document.getElementById(id)
+  this.context = this.canvas.getContext('2d')
+  this.size = {
+    'initial': {
+      'width': this.canvas.width,
+      'height': this.canvas.height
+    },
+    'current': {
+      'width': this.canvas.width,
+      'height': this.canvas.height
+    }
+  }
+  this.images = {}
+  this.setOptions(options)
+}
+
+Canvas.prototype.setOptions = function (options) {
+  this.options = Object.assign({}, this._defaultOptions, this.options || {}, options)
+  this.canvas.style.background = this.options.backgroundColor
+  this._setBackgroundImage()
+  this.canvas.style.border = this.options.border
+  this.canvas.style.cursor = this.options.cursor
+  this.context.globalCompositeOperation = this.options.globalCompositeOperationDefault
+}
+
+Canvas.prototype._setBackgroundImage = function (url = this.options.backgroundImage) {
+  this.options.backgroundImage = url
+  if (this.options.backgroundImage !== 'none') {
+    this.canvas.style.background = "url('" + this.options.backgroundImage + "')"
+  } else {
+    this.canvas.style.background = this.options.backgroundColor
+  }
+}
+
+Canvas.prototype.resize = function (width, height) {
+  this.canvas.width = width
+  this.canvas.height = height
+  this.size.current.width = width
+  this.size.current.height = height
+}
+
+Canvas.prototype.clear = function () {
+  this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+}
+
+Canvas.prototype.loadImage = function (src) {
+  if (!this.images.hasOwnProperty(src)) {
+    const image = new Image()
+    image.src = src
+    this.images[src] = image
+  }
+}
+
+Canvas.prototype.loadImages = function (sources) {
+  sources.forEach(src => this.loadImage(src))
+}
+
+Canvas.prototype._drawImage = function (
+  image, sx, sy, swidth, sheight, x, y, width, height, globalCompositeOperation
+) {
+  if (globalCompositeOperation) {
+    var globalCompositeOperationOriginal = this.context.globalCompositeOperation
+    this.context.globalCompositeOperation = globalCompositeOperation
+  }
+  this.context.drawImage(image, sx, sy, swidth, sheight, x, y, width, height)
+  if (globalCompositeOperation) {
+    this.context.globalCompositeOperation = globalCompositeOperationOriginal
+  }
+}
+
+Canvas.prototype._drawImageWhenLoaded = function (
+  image, sx, sy, swidth, sheight, x, y, width, height, globalCompositeOperation
+) {
+  if (image.complete) {
+    this._drawImage(image, sx, sy, swidth, sheight, x, y, width, height, globalCompositeOperation)
+  } else {
+    image.onload = () => {
+      this._drawImage(image, sx, sy, swidth, sheight, x, y, width, height, globalCompositeOperation)
+    }
+  }
+}
+
+Canvas.prototype.image = function (src, x, y, globalCompositeOperation) {
+  this.imageClipped(src, null, null, null, null, x, y, null, null, globalCompositeOperation)
+}
+
+Canvas.prototype.imageResized = function (src, x, y, width, height, globalCompositeOperation) {
+  this.imageClipped(src, null, null, null, null, x, y, width, height, globalCompositeOperation)
+}
+
+Canvas.prototype.imageClipped = function (
+  src, sx, sy, swidth, sheight, x, y, width, height, globalCompositeOperation
+) {
+  this.loadImage(src)
+  const image = this.images[src]
+  width = width || image.width
+  height = height || image.height
+  swidth = swidth || width
+  sheight = sheight || height
+  this._drawImageWhenLoaded(image, sx, sy, swidth, sheight, x, y, width, height, globalCompositeOperation)
+}
+
+Canvas.prototype.text = function (text, x, y, fillStyle, font, textAlign, textBaseline) {
+  this.textCompressed(text, x, y, undefined, fillStyle, font, textAlign, textBaseline)
+}
+
+Canvas.prototype.textCompressed = function (
+  text,
+  x,
+  y,
+  maxWidth,
+  fillStyle = this.options.color,
+  font = this.options.font,
+  textAlign = this.options.textAlign,
+  textBaseline = this.options.textBaseline
+) {
+  this.context.fillStyle = fillStyle
+  this.context.font = font
+  this.context.textAlign = textAlign
+  this.context.textBaseline = textBaseline
+  this.context.fillText(text, x, y, maxWidth)
+}
+
+Canvas.prototype.line = function (x1, y1, x2, y2, strokeStyle, strokeWidth) {
+  this.context.beginPath()
+  this.context.moveTo(x1, y1)
+  this.context.lineTo(x2, y2)
+  this._stylizeStroke(strokeStyle, strokeWidth)
+}
+
+Canvas.prototype.lines = function (lines, strokeStyle, strokeWidth) {
+  this.context.beginPath()
+  lines.forEach(line => {
+    this.context.moveTo(line[0], line[1])
+    this.context.lineTo(line[2], line[3])
+  })
+  this._stylizeStroke(strokeStyle, strokeWidth)
+}
+
+Canvas.prototype.polyLine = function (coords, strokeStyle, strokeWidth) {
+  this.context.beginPath()
+  this.context.moveTo(coords[0][0], coords[0][1])
+  for (let i = 1, len = coords.length; i < len; i++) {
+    this.context.lineTo(coords[i][0], coords[i][1])
+  }
+  this._stylizeStroke(strokeStyle, strokeWidth)
+}
+
+Canvas.prototype.quadraticCurve = function (x1, y1, x2, y2, qx, qy, strokeStyle, strokeWidth) {
+  this.context.beginPath()
+  this.context.moveTo(x1, y1)
+  this.context.quadraticCurveTo(qx, qy, x2, y2)
+  this._stylizeStroke(strokeStyle, strokeWidth)
+}
+
+Canvas.prototype.polygon = function (coords, fillStyle, strokeStyle, strokeWidth) {
+  this.context.beginPath()
+  this.context.moveTo(coords[0][0], coords[0][1])
+  coords.forEach(coord => {
+    this.context.lineTo(coord[0], coord[1])
+  })
+  this._stylize(fillStyle, strokeStyle, strokeWidth)
+}
+
+Canvas.prototype.circle = function (cx, cy, r, fillStyle, strokeStyle, strokeWidth) {
+  this.context.beginPath()
+  this.context.arc(cx, cy, r, 0, 2 * Math.PI, false)
+  this._stylize(fillStyle, strokeStyle, strokeWidth)
+}
+
+Canvas.prototype.rectangle = function (x, y, w, h, fillStyle, strokeStyle, strokeWidth) {
+  this.context.beginPath()
+  this.context.rect(x, y, w, h)
+  this._stylize(fillStyle, strokeStyle, strokeWidth)
+}
+
+Canvas.prototype.square = function (x, y, w, fillStyle, strokeStyle, strokeWidth) {
+  this.rectangle(x, y, w, w, fillStyle, strokeStyle, strokeWidth)
+}
+
+Canvas.prototype._stylizeFill = function (fillStyle = this.options.color) {
+  this.context.fillStyle = fillStyle
+  this.context.fill()
+}
+
+Canvas.prototype._stylizeStroke = function (strokeStyle = this.options.color, strokeWidth = '1') {
+  this.context.strokeStyle = strokeStyle
+  this.context.lineWidth = strokeWidth
+  this.context.stroke()
+}
+
+Canvas.prototype._stylize = function (fillStyle = this.options.color, strokeStyle, strokeWidth) {
+  this._stylizeFill(fillStyle)
+  this._stylizeStroke(strokeStyle || fillStyle, strokeWidth)
+}
+
+Canvas.prototype.getMousePosition = function () {
+  const rect = this.canvas.getBoundingClientRect()
+  return {
+    x: event.clientX - rect.left,
+    y: event.clientY - rect.top
+  }
+}
+
+Canvas.prototype._defaultOptions = {
+  'globalCompositeOperationDefault': 'source-over',
+  'backgroundColor': 'none',
+  'backgroundImage': 'none',
+  'color': '#000000',
+  'border': 'none',
+  'font': '16px Arial',
+  'textAlign': 'center',
+  'textBaseline': 'middle',
+  'cursor': 'default'
+}
+
+window.Canvas = Canvas
